621cebe82dd9469db24fa80c3c815ebe90b5cd95
amao <18390945534@163.com>
2019-04-15 11:06:02 +0800

    add a.c

---
+++ a.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int add(int a,int b);
+int sub(int a,int b);
+
+int main(void)
+{
+    int a = 3,b = 2,c;
+    int (*s[2])(int d,int f) = {add,sub};
+    c = (*s[0])(3,2);
+    printf("%d\n",c);
+    c = (*s[1])(3,2);
+    printf("%d\n",c);
+    return 0;
+}
+
+int add(int a,int b)
+{
+    return a+b;
+}
+
+int sub(int a,int b)
+{
+    return a-b;
+}
